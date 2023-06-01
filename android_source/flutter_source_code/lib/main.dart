74801cbc5133fca5b40a6cc6a22da893477695a0
Abimanyu <timorisu4@gmail.com>
2023-06-01 07:32:47 +0700

    init push

---
+++ android_source/flutter_source_code/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainClass());
+}
+
+class MainClass extends StatelessWidget {
+  const MainClass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
